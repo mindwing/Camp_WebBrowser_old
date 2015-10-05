18a39add06b10527ec415c9475861e4c7d2616be
mindwing <mindwing@gmail.com>
2015-10-05 21:11:00 +0900

    Initial commit

---
+++ app/src/main/java/android/camp/camp_webbrowser/MainActivity.java
@@ -0,0 +1,13 @@
+package android.camp.camp_webbrowser;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
